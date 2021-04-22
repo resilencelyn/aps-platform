b0446858f7812cba5b3bd895d1a888d55a8ba383
zhangbig0 <zhangquan0821@163.com>
2021-04-22 21:57:26 +0800

    修改手动调整部分2

--- aps-platform/Aps/Infrastructure/Repositories/ScheduleRecordRepository.cs
+++ aps-platform/Aps/Infrastructure/Repositories/ScheduleRecordRepository.cs
@@ -37,6 +37,8 @@ namespace Aps.Services
                 .ThenInclude(x => x.PreJob)
                 .Include(x => x.ApsAssemblyJobs)
                 .ThenInclude(x => x.ManufactureJobs)
+                .Include(x => x.Resources)
+                .ThenInclude(x => x.WorkJobs)
                 .FirstOrDefaultAsync(predicate);
         }
     }
